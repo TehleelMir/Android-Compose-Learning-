53ec486541c7a379a351cc83156fe5da4ca4d74d
TehleelMir <tehleelmir64@gmail.com>
2022-11-05 15:04:03 +0530

    basic of compose: https://developer.android.com/jetpack/compose/tutorial?gclid=CjwKCAjw8JKbBhBYEiwAs3sxN1OhCXMT4SSQUNornn-JlvzUUSdfuPscz5XnSiKRelsQBuXjYXTj6RoCORcQAvD_BwE&gclsrc=aw.ds done

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -1,21 +1,143 @@
 package com.example.composelearning
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composelearning.dataClasses.Message
+import com.example.composelearning.dataClasses.SampleData
 import com.example.composelearning.ui.theme.ComposeLearningTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLearningTheme {
+                Surface {
+                    Conversation(messageList = SampleData.conversationSample)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun MessageCard(message: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "Contact Profile Picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(
+                        1.5.dp,
+                        MaterialTheme.colors.secondary,
+                        CircleShape
+                    )
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            var isExpanded by remember {
+                mutableStateOf(false)
+            }
+
+            val surfaceColor by animateColorAsState(
+                if (isExpanded)
+                    MaterialTheme.colors.primary
+                else
+                    MaterialTheme.colors.surface
+            )
+
+            Column(
+                modifier = Modifier.clickable {
+                    isExpanded = !isExpanded
+                }
+            ) {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 2.dp,
+                    color = surfaceColor,
+                    modifier = Modifier
+                        .animateContentSize()
+                        .padding(1.dp)
+                ) {
+                    Text(
+                        text = message.body,
+                        style = MaterialTheme.typography.body2,
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Conversation(messageList: List<Message>) {
+        LazyColumn {
+            messageList.forEach {
+                item {
+                    MessageCard(message = it)
+                }
+            }
+        }
+    }
+
+    @Preview
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark mode"
+    )
+    @Composable
+    fun ConversationPreview() {
+        ComposeLearningTheme {
+            Surface {
+                Conversation(messageList = SampleData.conversationSample)
+            }
+        }
+    }
+
+    @Preview
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark mode"
+    )
+    @Composable
+    fun PreviewMessageCard() {
+        ComposeLearningTheme {
+            Surface {
+                MessageCard(Message("Tehleel", "Hello there!"))
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/composelearning/dataClasses/Message.kt
@@ -0,0 +1,14 @@
+package com.example.composelearning.dataClasses
+
+/**
+ * Created by Tehleel Mir
+ * Date: 05.11.2022
+ * Time: 11:09
+ *
+ * Description:
+ */
+
+data class Message(
+    val author: String,
+    val body: String,
+)
