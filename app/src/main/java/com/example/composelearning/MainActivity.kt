29234fa535d34be8f585082d151ba5b2b548c43c
TehleelMir <tehleelmir64@gmail.com>
2022-11-05 10:51:58 +0530

    init

---
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.composelearning
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composelearning.ui.theme.ComposeLearningTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}
