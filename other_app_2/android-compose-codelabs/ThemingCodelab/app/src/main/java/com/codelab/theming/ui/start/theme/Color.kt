0305fe1e65c7cef2d81550b507e6fb26fe6845d9
TehleelMir <tehleelmir64@gmail.com>
2022-11-28 10:14:58 +0530

    done

---
+++ other_app_2/android-compose-codelabs/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Created by Tehleel Mir
+ * Date: 25.11.2022
+ * Time: 10:17
+ *
+ * Description:
+ */
+
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
---
+++ other_app_2/android-compose-codelabs/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Shapes.kt
@@ -0,0 +1,20 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by Tehleel Mir
+ * Date: 25.11.2022
+ * Time: 12:21
+ *
+ * Description:
+ */
+
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
